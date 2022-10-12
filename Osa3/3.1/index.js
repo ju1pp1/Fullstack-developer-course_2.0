61f0fc86a18d847bd27da81c040d76ed52172e4d
ju1pp1 <jeremicraft@hotmail.com>
2022-10-12 16:34:36 +0300

    Code base ready before tasks.

--- Osa3/3.1/index.js
+++ Osa3/3.1/index.js
@@ -1,5 +1,8 @@
+//const http = require('http')
 const express = require('express')
 const app = express()
+
+app.use(express.json())
 
 let notes = [
     {
@@ -21,19 +24,92 @@ let notes = [
       important: true
     }
   ]
-  app.get('/', (req, res) => {
-    res.send('<h1>Hello world</h1>')
+/*
+  app.post('/api/notes', (request, response) => {
+    const note = request.body
+    console.log(note)
+    response.json(note)
   })
+  
+  //DEL
+  app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+    response.status(204).end()
+  })
+    */
+    //GET
+    //Nyt yksittäisen resurssin hakeminen toimii.
+    /*
+    app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+    if (note) {
+      response.json(note)
+    } else {
+      response.status(404).end()
+    }
+    })
+    */
+  /*
   app.get('/api/notes', (req, res) => {
     res.json(notes)
   })
-/*
-const app = http.createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'application/json' })
+  */
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello world</h1>')
     response.end(JSON.stringify(notes))
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if(note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+}) 
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
+})
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+  //Jos poisto onnistuu vastataan statuskoodilla 204.
+  response.status(204).end()
+})
+const generateId = () => {
+  const maxId = notes.length > 0
+  ? Math.max(...notes.map(n => n.id))
+  : 0
+return maxId + 1
+}
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+  if(!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+  const note = {
+    content: body.conntent,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+  notes = notes.concat(note)
+  response.json(note)
+})
+/*
+app.get('/api/notes', (request, response) => {
+  response.json(notes)
 })
 */
 const PORT = 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
+  
